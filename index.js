51674461409c81cf05a39619377a1797b6e05514
Tiago Garbi <tgbmateriais@gmail.com>
2024-07-16 15:22:47 -0300

    feature: include mock test code

---
+++ index.js
@@ -0,0 +1,40 @@
+[
+    {
+      id: '1',
+      ' name': ' john doe',
+      ' profession': ' developer back end',
+      ' age': ' 43'
+    },
+    {
+      id: '2',
+      ' name': ' jane doe',
+      ' profession': ' developer front end',
+      ' age': ' 35'
+    },
+    {
+      id: '3',
+      ' name': ' jasmin doe',
+      ' profession': ' developer UX/UI',
+      ' age': ' 27'
+    }
+  ],
+  expected: [
+    {
+      id: 1,
+      name: 'john doe',
+      profession: 'developer back end',
+      age: 43
+    },
+    {
+      id: 2,
+      name: 'jane doe',
+      profession: 'developer front end',
+      age: 35
+    },
+    {
+      id: 3,
+      name: 'jasmin doe',
+      profession: 'developer UX/UI',
+      age: 27
+    }
+  ],
