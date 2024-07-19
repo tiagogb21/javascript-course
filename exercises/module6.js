6a80a068eda294f85be68488710b294347135451
Tiago Garbi <tgbmateriais@gmail.com>
2024-07-19 10:46:46 -0300

    feature: include exercise

---
+++ exercises/module6.js
@@ -0,0 +1,26 @@
+const numbers = Array.from(
+    { length: 10 },
+    (_, i) => Math.round(Math.random() * 100)
+);
+
+console.log(numbers);
+
+// Find the smallest number in an array
+
+// Exercise solution:
+
+const smallest = numbers.reduce((acc, curr) => {
+    return curr < acc ? curr : acc;
+});
+
+console.log(smallest);
+
+// Find the biggest number in an array
+
+// Exercise solution:
+
+const biggest = numbers.reduce((acc, curr) => {
+    return curr > acc ? curr : acc;
+});
+
+console.log(biggest);
